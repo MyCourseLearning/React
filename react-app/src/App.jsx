ac68785e8eeb34f70ddcd82ba9d28b33608b6b21
Nxwbtk <65420147@kmitl.ac.th>
2025-05-18 23:49:55 +0700

    Add Layout component with Topbar and Footer; update App to use Layout

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import AboutPage from "./components/about";
 import HomePage from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={null}>
+          <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
